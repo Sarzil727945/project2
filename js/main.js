aa82d300d13fa46f7221025b9abaa882c7039427
Sarzil727945 <sarzilmuntaha.gmail.com>
2022-12-02 21:33:21 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+                                            // navber js part start
+
+$(document).ready(function(){
+    $(".nav1 a").click(function(){
+        $(this).css("color","red");
+    })
+
+    $(".nav1 a").blur(function(){
+        $(this).css("color","black");
+    })
+        
+})
+
+                                            // navber js part ends
